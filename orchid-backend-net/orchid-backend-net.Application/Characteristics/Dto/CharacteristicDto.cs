97f73d38cb9171fd7157449e14c2cf8e6028007b
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2026-01-01 19:49:55 +0700

    feat: get characteristic to create seedlings

---
+++ orchid-backend-net/orchid-backend-net.Application/Characteristics/Dto/CharacteristicDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using orchid_backend_net.Application.Common.Mappings;
+using orchid_backend_net.Domain.Entities;
+
+namespace orchid_backend_net.Application.Characteristics.Dto
+{
+    public class CharacteristicDto : IMapFrom<Characteristic>
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+        public string Unit { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Characteristic, CharacteristicDto>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.ID.ToString()));
+        }
+    }
+}
---
+++ orchid-backend-net/orchid-backend-net.Application/Characteristics/GetAllCharacteristic/GetAllCharacteristicQuery.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using orchid_backend_net.Application.Characteristics.Dto;
+using orchid_backend_net.Application.Common.Extension;
+using orchid_backend_net.Application.Common.Pagination;
+using orchid_backend_net.Domain.IRepositories;
+
+namespace orchid_backend_net.Application.Characteristics.GetAllCharacteristic
+{
+    public class GetAllCharacteristicQuery : IRequest<PageResult<CharacteristicDto>>
+    {
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public GetAllCharacteristicQuery() { }
+        public GetAllCharacteristicQuery(int pageNo, int pageSize)
+        {
+            PageNo = pageNo;
+            PageSize = pageSize;
+        }
+    }
+
+    internal class GetAllCharacteristicQueryHandler(ICharacteristicRepository characteristicRepository) : IRequestHandler<GetAllCharacteristicQuery, PageResult<CharacteristicDto>>
+    {
+        public async Task<PageResult<CharacteristicDto>> Handle(GetAllCharacteristicQuery request, CancellationToken cancellationToken)
+        {
+            var characteristics = await characteristicRepository.FindAllProjectToAsync<CharacteristicDto>(pageNo: request.PageNo, pageSize: request.PageSize, cancellationToken: cancellationToken);
+            return characteristics.ToAppPageResult();
+        }
+    }
+}

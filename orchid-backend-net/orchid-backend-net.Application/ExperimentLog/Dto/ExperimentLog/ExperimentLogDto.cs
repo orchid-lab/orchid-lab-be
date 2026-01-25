f2b1602831a5dd8e201f8a16577284174f41a9f8
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2026-01-26 00:58:37 +0700

    fix: experiment log dto contains status

--- orchid-backend-net/orchid-backend-net.Application/ExperimentLog/Dto/ExperimentLog/ExperimentLogDto.cs
+++ orchid-backend-net/orchid-backend-net.Application/ExperimentLog/Dto/ExperimentLog/ExperimentLogDto.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using orchid_backend_net.Application.Common.Extension;
 using orchid_backend_net.Application.Common.Mappings;
 using orchid_backend_net.Domain.Entities;
 
@@ -14,14 +15,16 @@ namespace orchid_backend_net.Application.ExperimentLog.Dto.ExperimentLog
         public int ExpectedSampleCount { get; set; }
         public string CreatedBy { get; set; }   
         public DateTime CreatedDate { get; set; }
-
+        public string Status { get; set; }
         public void Mapping(Profile profile)
         {
             profile.CreateMap<ExperimentLogs, ExperimentLogDto>()
                 .ForMember(dest => dest.MethodName, 
                 opt => opt.MapFrom(src => src.Method.Name))
                 .ForMember(dest => dest.BatcheName,
-                opt => opt.MapFrom(src => src.Batch.BatchName));
+                opt => opt.MapFrom(src => src.Batch.BatchName))
+                .ForMember(dest => dest.Status,
+                opt => opt.MapFrom(src => src.Status.ToDisplayText()));
         }
     }
 }

35d5482f72e29f7cef970a87dbf3e8120b40d205
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2026-01-27 02:32:40 +0700

    feat: monitoring dto

---
+++ orchid-backend-net/orchid-backend-net.Application/MonitoringLog/Dto/AnalyticResult/AnalyticResultDto.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using orchid_backend_net.Application.Common.Mappings;
+
+namespace orchid_backend_net.Application.MonitoringLog.Dto.AnalyticResult
+{
+    public class AnalyticResultDto : IMapFrom<Domain.Entities.AnalyticResults>
+    {
+        public required decimal Anthracnose { get; set; }
+        public required decimal BacterialWilt { get; set; }
+        public required decimal Blackrot { get; set; }
+        public required decimal Brownspots { get; set; }
+        public required decimal MoldBacterial { get; set; }
+        public required decimal MoldFungus { get; set; }
+        public required decimal SoftRot { get; set; }
+        public required decimal StemRot { get; set; }
+        public required decimal WitheredYellowRoot { get; set; }
+        public required decimal Healthy { get; set; }
+        public required decimal Oxidation { get; set; }
+        public required decimal Virus { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Entities.AnalyticResults, AnalyticResultDto>();
+        }
+    }
+}
---
+++ orchid-backend-net/orchid-backend-net.Application/MonitoringLog/Dto/MonitoringLog/MonitoringLogDetailDto.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using orchid_backend_net.Application.Common.Mappings;
+using orchid_backend_net.Application.MonitoringLog.Dto.AnalyticResult;
+using orchid_backend_net.Application.MonitoringLog.Dto.LogDetail;
+using orchid_backend_net.Domain.Common.Enum;
+
+namespace orchid_backend_net.Application.MonitoringLog.Dto.MonitoringLog
+{
+    public class MonitoringLogDetailDto : IMapFrom<Domain.Entities.MonitoringLogs>
+    {
+        public required string Id { get; set; }
+        public required string CreatedBy { get; set; }
+        public required DateOnly CreatedDate { get; set; }
+        public string SampleName { get; set; } = null!;
+        public string SampleStageDefinitionName { get; set; } = null!;
+        public string? DiseaseName { get; set; }
+        public AnalyticResultDto? AnalyticResult { get; set; }
+        public MonitoringLogStatus Status { get; set; }
+        public DateOnly? DeletedDate { get; set; }
+        public string? DeletedBy { get; set; }
+        public DateOnly? UpdateBy { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool IsNewest { get; set; }
+        public List<LogDetailDto> LogDetails { get; set; } = new();
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Entities.MonitoringLogs, MonitoringLogDetailDto>()
+                .ForMember(dest => dest.SampleName, 
+                    opt => opt.MapFrom(
+                        src => src.SampleStage.Samples.Name))
+                .ForMember(dest => dest.CreatedDate, 
+                    opt => opt.MapFrom(
+                        src => DateOnly.FromDateTime(src.CreatedDate)))
+                .ForMember(dest => dest.SampleStageDefinitionName, 
+                    opt => opt.MapFrom(
+                        src => src.SampleStage.SampleStageDefinition.Name));
+        }
+    }
+}

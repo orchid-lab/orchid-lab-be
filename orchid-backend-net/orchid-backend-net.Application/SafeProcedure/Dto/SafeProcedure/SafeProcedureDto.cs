f64d2de6074d14661a938b0a729c41012b481efe
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2026-02-09 20:08:37 +0700

    feat: safe procedure dto

--- orchid-backend-net/orchid-backend-net.Application/SafeProcedure/Dto/SafeProcedure/SafeProcedureDto.cs
+++ orchid-backend-net/orchid-backend-net.Application/SafeProcedure/Dto/SafeProcedure/SafeProcedureDto.cs
@@ -1,6 +1,18 @@
-﻿namespace orchid_backend_net.Application.SafeProcedure.Dto.SafeProcedure
+﻿using AutoMapper;
+using orchid_backend_net.Application.Common.Mappings;
+
+namespace orchid_backend_net.Application.SafeProcedure.Dto.SafeProcedure
 {
-    public class SafeProcedureDto
+    public class SafeProcedureDto : IMapFrom<Domain.Entities.SafeProcedure>
     {
+        public required string Id { get; set; } = default!;
+        public string ProcedureName { get; set; } = default!;
+        public int StepNumber { get; set; }
+        public string Description { get; set; } = default!;
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Entities.SafeProcedure, SafeProcedureDto>();
+        }
     }
 }

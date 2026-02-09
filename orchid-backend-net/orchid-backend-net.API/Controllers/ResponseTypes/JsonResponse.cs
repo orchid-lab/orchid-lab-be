8297b8d9c9217cbe26e1024fb1ad7ef39588920b
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2026-02-09 19:50:16 +0700

    feat: config api

--- orchid-backend-net/orchid-backend-net.API/Controllers/ResponseTypes/JsonResponse.cs
+++ orchid-backend-net/orchid-backend-net.API/Controllers/ResponseTypes/JsonResponse.cs
@@ -1,7 +1,15 @@
 ﻿namespace orchid_backend_net.API.Controllers.ResponseTypes
 {
+    /// <summary>
+    /// json response type for api responses
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="value"></param>
     public class JsonResponse<T>(T value)
     {
+        /// <summary>
+        /// value by pass as generic
+        /// </summary>
         public T Value { get; set; } = value;
     }
 }
---
+++ orchid-backend-net/orchid-backend-net.API/Dto/Config/UpdateConfigDto.cs
@@ -0,0 +1,10 @@
+﻿namespace orchid_backend_net.API.Dto.Config
+{
+    /// <summary>
+    /// update config dto
+    /// </summary>
+    /// <param name="ConfigName"></param>
+    /// <param name="Key"></param>
+    /// <param name="Value"></param>
+    public record UpdateConfigDto(string? ConfigName, string? Key, decimal? Value);
+}

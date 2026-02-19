63590a475e105c774f71a9e52a2f1125ca4ea059
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2026-02-19 19:29:54 +0700

    fix/bug: remove temporary about validation

--- orchid-backend-net/orchid-backend-net.Application/Tasks/UseCase/TechnicianSubmitTaskCheckList/TechnicianSubmitTaskCheckListItemCommandValidator.cs
+++ orchid-backend-net/orchid-backend-net.Application/Tasks/UseCase/TechnicianSubmitTaskCheckList/TechnicianSubmitTaskCheckListItemCommandValidator.cs
@@ -8,8 +8,6 @@ namespace orchid_backend_net.Application.Tasks.UseCase.TechnicianSubmitTaskCheck
         {
             RuleFor(x => x.TaskId).NotEmpty().WithMessage("TaskId không được để trống");
             RuleFor(x => x.ItemId).NotEmpty().WithMessage("ItemId không được để trống");
-            RuleFor(x => x.MeasurementUnit).NotEmpty().WithMessage("MeasurementUnit không được để trống");
-            RuleFor(x => x.MeasuredValue).GreaterThanOrEqualTo(0).WithMessage("MeasuredValue phải lớn hơn hoặc bằng 0");
         }
     }
 }

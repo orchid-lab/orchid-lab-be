0d2c9067a4025137bbd72d2252ee2200475be832
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2026-03-03 16:18:53 +0700

    feat: get all image api

--- orchid-backend-net/orchid-backend-net.Application/Images/Dto/Img/ImageDto.cs
+++ orchid-backend-net/orchid-backend-net.Application/Images/Dto/Img/ImageDto.cs
@@ -9,12 +9,14 @@ namespace orchid_backend_net.Application.Images.Dto.Img
         public string Id { get; set; } = null!;
         public ImageTargetType TargetType { get; set; }
         public string TargetId { get; set; } = null!;
+        public string Url { get; set; } = null!;
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Domain.Entities.Imgs, ImageDto>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ID))
                 .ForMember(dest => dest.TargetType, opt => opt.MapFrom(src => src.TargetType))
-                .ForMember(dest => dest.TargetId, opt => opt.MapFrom(src => src.TargetId));
+                .ForMember(dest => dest.TargetId, opt => opt.MapFrom(src => src.TargetId))
+                .ForMember(dest => dest.Url, opt => opt.MapFrom(src => src.Url));
         }
     }
 }

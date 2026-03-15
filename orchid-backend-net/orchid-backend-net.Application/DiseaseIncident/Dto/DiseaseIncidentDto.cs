f2563cbba1f96e15c57a826ed58e9af7941e8bd6
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2026-03-16 02:32:56 +0700

    feat: review incident now has notification

--- orchid-backend-net/orchid-backend-net.Application/DiseaseIncident/Dto/DiseaseIncidentDto.cs
+++ orchid-backend-net/orchid-backend-net.Application/DiseaseIncident/Dto/DiseaseIncidentDto.cs
@@ -8,6 +8,7 @@ namespace orchid_backend_net.Application.DiseaseIncident.Dto
     {
         public string Id { get; set; } = default!;
         public string SampleStageId { get; set; } = default!;
+        public string SampleName { get; set; } = default!;
         public string DiseaseName { get; set; } = default!;
         public DiseaseIncidentStatus Status { get; set; }
         public decimal AIConfidence { get; set; }
@@ -16,7 +17,8 @@ namespace orchid_backend_net.Application.DiseaseIncident.Dto
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Domain.Entities.DiseaseIncident, DiseaseIncidentDto>()
-                .ForMember(dest => dest.DiseaseName, opt => opt.MapFrom(src => src.Disease != null ? src.Disease.Name : string.Empty));
+                .ForMember(dest => dest.DiseaseName, opt => opt.MapFrom(src => src.Disease != null ? src.Disease.Name : string.Empty))
+                .ForMember(dest => dest.SampleName, opt => opt.MapFrom(src => src.SampleStage.Samples.Name));
         }
     }
 }

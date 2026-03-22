004af37e16d3135d90a843bf0fee3085c1e306fd
Lam Do <84032766+lama-try-code@users.noreply.github.com>
2026-03-22 22:08:48 +0700

    feat: cancel task in destroy experiment log

---
+++ orchid-backend-net/orchid-backend-net.Domain/Events/TaskEvents/TaskCancelledEvent.cs
@@ -0,0 +1,7 @@
+﻿using orchid_backend_net.Domain.Events.Base;
+
+namespace orchid_backend_net.Domain.Events.TaskEvents
+{
+    public record TaskCancelledEvent(string TaskId, string CancelledBy, string Technician,string? Reason)
+        : DomainEvent;
+}

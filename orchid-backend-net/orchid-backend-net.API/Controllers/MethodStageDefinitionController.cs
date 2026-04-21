0f85de57a5a76f5f9d0ec8b35518c8afc6c4495b
ThanhIsHereee <nquangthanh88@gmail.com>
2026-04-21 21:42:55 +0700

    bug/fix: error 500 api getall

--- orchid-backend-net/orchid-backend-net.API/Controllers/MethodStageDefinitionController.cs
+++ orchid-backend-net/orchid-backend-net.API/Controllers/MethodStageDefinitionController.cs
@@ -20,7 +20,7 @@ using orchid_backend_net.Application.MethodStageDefinition.UseCase.UpdateMethodS
 namespace orchid_backend_net.API.Controllers
 {
     /// <summary>
-    /// chemical api
+    /// method stage definition api
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="logger"></param>
@@ -74,79 +74,5 @@ namespace orchid_backend_net.API.Controllers
                 return BadRequest(new ProblemDetails { Title = "Lấy dữ liệu thất bại", Detail = ex.Message });
             }
         }
-        
-        ///// <summary>
-        ///// create chemical only admin can use this
-        ///// </summary>
-        ///// <param name="command"></param>
-        ///// <param name="cancellationToken"></param>
-        ///// <returns></returns>
-        //[HttpPost]
-        //[ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-        //[ProducesResponseType(typeof(string), StatusCodes.Status201Created)]
-        //public async Task<ActionResult<JsonResponse<string>>> CreateChemical([FromBody] CreateMethodStageDefinitionCommand command, CancellationToken cancellationToken = default)
-        //{
-        //    try
-        //    {
-        //        logger.LogInformation("Received POST request at {Time}", DateTime.UtcNow);
-        //        var result = await Sender.Send(command, cancellationToken);
-        //        return CreatedAtAction(nameof(GetChemicalById), new { id = result }, new JsonResponse<string>(result));
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        logger.LogError(ex, "Error occurred while creating method stage definition.");
-        //        return BadRequest(new ProblemDetails { Title = "Tạo thất bại", Detail = ex.Message });
-        //    }
-        //}
-
-        ///// <summary>
-        ///// delete chemical only admin can use this
-        ///// </summary>
-        ///// <param name="id"></param>
-        ///// <param name="cancellationToken"></param>
-        ///// <returns></returns>
-        //[HttpDelete("{id}")]
-        //[ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-        //public async Task<ActionResult<JsonResponse<string>>> DeleteChemical([FromRoute] int id, CancellationToken cancellationToken = default)
-        //{
-        //    try
-        //    {
-        //        logger.LogInformation("Received DELETE request at {Time}", DateTime.UtcNow);
-        //        var result = await Sender.Send(new DeleteMethodStageDefinitionCommand(id), cancellationToken);
-        //        return Ok(result);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        logger.LogError(ex, "Error occurred while deleting chemical.");
-        //        return BadRequest(new ProblemDetails { Title = "Xóa thất bại", Detail = ex.Message });
-        //    }
-        //}
-
-        ///// <summary>
-        ///// update chemical only admin can use this
-        ///// </summary>
-        ///// <param name="id"></param>
-        ///// <param name="dto"></param>
-        ///// <param name="cancellationToken"></param>
-        ///// <returns></returns>
-        //[HttpPut("{id}")]
-        //[ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-        //public async Task<ActionResult<JsonResponse<string>>> UpdateChemical(
-        //    [FromRoute] int id,
-        //    [FromBody] UpdateChemicalDto dto,
-        //    CancellationToken cancellationToken = default)
-        //{
-        //    try
-        //    {
-        //        logger.LogInformation("Received DELETE request at {Time}", DateTime.UtcNow);
-        //        var result = await Sender.Send(new UpdateMethodStageDefinitionCommand(id, dto.Name, dto.Description), cancellationToken);
-        //        return Ok(result);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        logger.LogError(ex, "Error occurred while deleting chemical.");
-        //        return BadRequest(new ProblemDetails { Title = "Cập nhật thất bại", Detail = ex.Message });
-        //    }
-        //}
     }
 }

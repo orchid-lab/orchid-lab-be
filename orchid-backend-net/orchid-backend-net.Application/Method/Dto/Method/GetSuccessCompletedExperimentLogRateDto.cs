0360449b9679ea055343280c3d4b8ba8ea0944f5
ThanhIsHereee <nquangthanh88@gmail.com>
2026-05-06 17:04:29 +0700

    fix: dto conflict not reconization

--- orchid-backend-net/orchid-backend-net.Application/Method/Dto/Method/GetSuccessCompletedExperimentLogRateDto.cs
+++ orchid-backend-net/orchid-backend-net.Application/Method/Dto/Method/GetSuccessCompletedExperimentLogRateDto.cs
@@ -6,7 +6,6 @@ using orchid_backend_net.Domain.Entities;
 namespace orchid_backend_net.Application.Method.Dto.Method
 {
     public class GetSuccessCompletedExperimentLogRateDto
-        : IMapFrom<Domain.Entities.Methods>
     {
         public int Id { get; set; }
         public required string Name { get; set; }
@@ -17,22 +16,5 @@ namespace orchid_backend_net.Application.Method.Dto.Method
         public int? SuccessRate { get; set; }
         public List<Seedlings>? Seedling { get; set; }
         public List<Domain.Entities.MethodStageDefinition>? MethodStages { get; set; }
-        public void Mapping(Profile profile)
-        {
-            profile.CreateMap<Methods, GetSuccessCompletedExperimentLogRateDto>()
-               .ForMember(dest => dest.TotalDurationDays,
-               opt => opt.MapFrom(
-                   src => src.MethodStages.Sum(ms => ms.DurationsDays)))
-               //.ForMember(dest => dest.CompletedExperimentLog,
-               //opt => opt.MapFrom(
-               //    src => src.ExperimentLogs.Count(el => el.Status == ExperimentLogStatus.Completed && el.MethodId == this.Id)))
-               //.ForMember(dest => dest.FailedExperimentLog,
-               //opt => opt.MapFrom(
-               //    src => src.ExperimentLogs.Count(el => el.Status == ExperimentLogStatus.Cancelled && el.MethodId == this.Id)))
-               //.ForMember(dest => dest.MethodStages,
-               //opt => opt.MapFrom(
-               //    src => src.ExperimentLogs.Where(el => el.MethodId.Equals(this.Id) && el.Status.Equals(ExperimentLogStatus.Cancelled))))
-               ;
-        }
     }
 }

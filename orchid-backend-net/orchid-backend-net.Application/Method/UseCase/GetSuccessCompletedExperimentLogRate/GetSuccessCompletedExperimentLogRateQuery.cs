6373d77b08aa65ee95beebc6f44613e259e73688
ThanhIsHereee <nquangthanh88@gmail.com>
2026-05-07 05:31:18 +0700

    feat: add seedling

--- orchid-backend-net/orchid-backend-net.Application/Method/UseCase/GetSuccessCompletedExperimentLogRate/GetSuccessCompletedExperimentLogRateQuery.cs
+++ orchid-backend-net/orchid-backend-net.Application/Method/UseCase/GetSuccessCompletedExperimentLogRate/GetSuccessCompletedExperimentLogRateQuery.cs
@@ -7,19 +7,13 @@ namespace orchid_backend_net.Application.Method.UseCase.GetSuccessCompletedExper
 {
     public class GetSuccessCompletedExperimentLogRateQuery : IRequest<List<GetSuccessCompletedExperimentLogRateDto>>
     {
-        //public int PageNumber { get; set; }
-        //public int PageSize { get; set; }
         public GetSuccessCompletedExperimentLogRateQuery() { }
-        //public GetSuccessCompletedExperimentLogRateQuery(int pageNumber, int pageSize)
-        //{
-        //    PageNumber = pageNumber;
-        //    PageSize = pageSize;
-        //}
     }
     internal class GetSuccessCompletedExperimentLogRateQueryHandler(
         IExperimentLogRepository experimentLogRepository,
         IMethodStageDefinitionRepository methodStageDefinitionRepository,
-        IMethodStageRepository methodStageRepository) :
+        IMethodStageRepository methodStageRepository,
+        ISeedlingRepository seedlingRepository) :
         IRequestHandler<GetSuccessCompletedExperimentLogRateQuery, List<GetSuccessCompletedExperimentLogRateDto>>
     {
         public async Task<List<GetSuccessCompletedExperimentLogRateDto>> Handle(GetSuccessCompletedExperimentLogRateQuery request, CancellationToken cancellationToken)
@@ -35,16 +29,19 @@ namespace orchid_backend_net.Application.Method.UseCase.GetSuccessCompletedExper
                     var completedExperimentLog = g.Count(x => x.Status == ExperimentLogStatus.Completed);
                     var failedExperimentLog = g.Where(x => x.Status == ExperimentLogStatus.Cancelled || x.Status == ExperimentLogStatus.Destroyed).ToList();
                     List<Domain.Entities.MethodStageDefinition> methodStages = new List<Domain.Entities.MethodStageDefinition>();
-                    //List<Domain.Entities.Seedlings> seedlings = new List<Domain.Entities.Seedlings>();
+                    List<Domain.Entities.Seedlings> seedlings = new List<Domain.Entities.Seedlings>();
                     foreach (var item in failedExperimentLog)
                     {
                         var methodStage = await methodStageRepository.FindAsync(x => x.ID.Equals(item.CurrentStageOrder), cancellationToken);
-                        if(methodStage != null) 
+                        if (methodStage != null)
                         {
                             var methodStageDefinition = await methodStageDefinitionRepository.FindAsync(x => x.ID.Equals(methodStage.MethodStageDefinitionId), cancellationToken);
                             if(methodStageDefinition != null)
                                 methodStages.Add(methodStageDefinition);
                         }
+                        var seedling = await seedlingRepository.FindAsync(x => x.ID.Equals(item.SeedlingParentId), cancellationToken);
+                            if(seedling != null)
+                                seedlings.Add(seedling);
                     }
                     //foreach (var item in g.Where(x => x.Status == ExperimentLogStatus.Cancelled || x.Status == ExperimentLogStatus.Destroyed).ToList())
                     //{
@@ -62,7 +59,7 @@ namespace orchid_backend_net.Application.Method.UseCase.GetSuccessCompletedExper
                         FailedExperimentLog = failedExperimentLog.Count(),
                         SuccessRate = completedExperimentLog + failedExperimentLog.Count() > 0 ? (int)((double)completedExperimentLog / (completedExperimentLog + failedExperimentLog.Count()) * 100) : 0,
                         MethodStages = methodStages,
-                        //Seedling = seedlings,
+                        Seedling = seedlings,
                         //TotalExperimentLog = totalExperimentLogs
                     };
                 });

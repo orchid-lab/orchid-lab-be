be2ee0d21261d0832c97063b32a44d2a6b2d7d48
Phát Tài Tăng Nguyễn <92193419+tykyfatkie@users.noreply.github.com>
2026-06-06 17:49:41 +0700

    Revert "view all diseases include inactive diseases"
    This reverts commit 1522bf5b877571232b177bb324c3ced7f5361591.

--- orchid-backend-net/orchid-backend-net.Application/Disease/UseCase/GetAllDisease/GetAllDiseaseQueryHandler.cs
+++ orchid-backend-net/orchid-backend-net.Application/Disease/UseCase/GetAllDisease/GetAllDiseaseQueryHandler.cs
@@ -2,6 +2,7 @@
 using orchid_backend_net.Application.Common.Pagination;
 using orchid_backend_net.Application.Disease.Dto;
 using orchid_backend_net.Domain.IRepositories;
+
 namespace orchid_backend_net.Application.Disease.UseCase.GetAllDisease
 {
     internal class GetAllDiseaseQueryHandler(IDiseaseRepository repo)
@@ -13,7 +14,8 @@ namespace orchid_backend_net.Application.Disease.UseCase.GetAllDisease
             return await repo.FindAllProjectToAsync<DiseaseDto>(
                 pageNo: request.PageNo,
                 pageSize: request.PageSize,
-                queryOptions: q => q.OrderByDescending(d => d.CreatedAt),
+                queryOptions: q => q.Where(d => d.IsActive)
+                                    .OrderByDescending(d => d.CreatedAt),
                 cancellationToken: ct
             );
         }

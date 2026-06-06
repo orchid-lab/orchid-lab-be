62a3c432663098ad682524de9e59984359cdf6d6
Phát Tài Tăng Nguyễn <92193419+tykyfatkie@users.noreply.github.com>
2026-06-06 17:50:29 +0700

    view all diseases include inactive diseases

--- orchid-backend-net/orchid-backend-net.Application/Disease/UseCase/GetAllDisease/GetAllDiseaseQueryHandler.cs
+++ orchid-backend-net/orchid-backend-net.Application/Disease/UseCase/GetAllDisease/GetAllDiseaseQueryHandler.cs
@@ -2,7 +2,6 @@
 using orchid_backend_net.Application.Common.Pagination;
 using orchid_backend_net.Application.Disease.Dto;
 using orchid_backend_net.Domain.IRepositories;
-
 namespace orchid_backend_net.Application.Disease.UseCase.GetAllDisease
 {
     internal class GetAllDiseaseQueryHandler(IDiseaseRepository repo)
@@ -14,8 +13,7 @@ namespace orchid_backend_net.Application.Disease.UseCase.GetAllDisease
             return await repo.FindAllProjectToAsync<DiseaseDto>(
                 pageNo: request.PageNo,
                 pageSize: request.PageSize,
-                queryOptions: q => q.Where(d => d.IsActive)
-                                    .OrderByDescending(d => d.CreatedAt),
+                queryOptions: q => q.OrderByDescending(d => d.CreatedAt),
                 cancellationToken: ct
             );
         }
